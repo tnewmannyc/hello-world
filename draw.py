6be9a5c04d619ac274da39c7f38bbc23fedbefdc
tnewmannyc <tatsiana.newman@gmail.com>
2023-09-28 22:33:37 -0400

    add new files

---
+++ draw.py
@@ -0,0 +1,14 @@
+import turtle
+painter = turtle.Turtle()
+<<<<<<< HEAD
+painter.pensize(20)
+
+for i in range(18):
+	painter.forward(100)
+	painter.left(100)
+	
+=======
+painter.forward(100)
+painter.left(90)
+>>>>>>> parent of 450def7 (repeat 4 times)
+turtle.done( )
